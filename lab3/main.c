3a338a8a1d235111947a8cfcddb82c9e859668e1
TheMadius <dima.pustovskikh@mail.ru>
2021-07-29 17:17:09 +0700

    add lab3

---
+++ lab3/main.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "stdbool.h"
+#include "string.h"
+#include "locale.h"
+
+
+int main()
+{
+    setlocale(LC_ALL,"Russian");
+    while(true)
+    {
+        printf("1 - Ввод новой записи");
+    }
+}
