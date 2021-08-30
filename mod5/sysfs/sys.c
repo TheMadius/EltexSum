05e75d6cfd257df931f76d908ac3906fe447cc2d
TheMadius <dima.pustovskikh@mail.ru>
2021-08-31 00:52:45 +0700

    fix error

--- mod5/sysfs/sys.c
+++ mod5/sysfs/sys.c
@@ -1,8 +1,8 @@
 #include <linux/module.h>
-#include <linux/printk.h>
-#include <linux/kobject.h>
-#include <linux/sysfs.h>
-#include <linux/init.h>
+#include "linux/printk.h"
+#include "linux/kobject.h"
+#include "linux/sysfs.h"
+#include "linux/init.h"
 #include <linux/fs.h>
 #include <linux/string.h>
 
@@ -11,10 +11,10 @@ static int foo;
 
 static ssize_t foo_show(struct kobject *kobj, struct kobj_attribute *attr, char *buf)
 {
-        return sprintf(buf, "%d\n", foo);
+        return sprintf(buf, "count:%d\n", foo);
 }
 
-static ssize_t foo_store(struct kobject *kobj, struct kobj_attribute *attr, char *buf, size_t count)
+static ssize_t foo_store(struct kobject *kobj, struct kobj_attribute *attr,const char *buf, size_t count)
 {
         sscanf(buf, "%du", &foo);
         return count;
